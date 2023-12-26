31742c37711fc49a6c5950ed0e73c66ebf6f23f9
pritesh-52 <priteshbhatiya52@gmail.com>
2023-12-26 21:57:47 +0530

    Eighth_commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes/router");
+const path=require("path");
 
 const app = express();
 require("./db/conn");
@@ -11,6 +12,8 @@ app.use(router);
 
 
 app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname,'client','build')))
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     res.send("Hello Pritesh");
 })
 app.listen(8000, () => {
